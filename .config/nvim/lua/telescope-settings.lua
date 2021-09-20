bfd753cc56557024f5cabd839345a756628d35f6
Tatsuki Ishihara <tatsukiishihara@TatsukinoMacBook-Air.local>
2021-09-20 23:31:21 +0900

    feat: Add neovim plugins

--- .config/nvim/lua/telescope-settings.lua
+++ .config/nvim/lua/telescope-settings.lua
@@ -1,4 +1,6 @@
+require'telescope'.load_extension'ghq'
 map('n','<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>')
 map('n','<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>')
 map('n','<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>')
 map('n','<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>')
+map('n','<leader>fq', '<cmd>lua require("telescope").extensions.ghq.list()<cr>')
